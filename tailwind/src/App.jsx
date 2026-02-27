39cd15ab333bb5e494804d067d0c6b706a383038
Amir Rehman Marwat <amirrehmanmwt@gmail.com>
2026-02-26 22:30:08 -0800

    Revised some most usefull css concept with practical implementation

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <nav className='bg-[#4592D1] flex justify-between  justify-around h-[50px] w-fit'>
+      <div className='pl-[50px] '>THE MENTORS</div>
+      <ul className='flex pr-[50px] box-border justify-between gap-[70px]'>
+        <li>HOME</li>
+        <li>SHOP</li>
+        <li>SALE</li>
+        <li>ABOUT</li>
+      </ul>
+    </nav>
+    </>
+  )
+}
+
+export default App
